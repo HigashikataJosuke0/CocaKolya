9af516ed564c2cc50c307803618a183368cc92b2
dante <dante276@yandex.ru>
2024-03-01 22:01:52 +0300

    it' work

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,35 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.service.UserServiceFind;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+    private final UserServiceFind userServiceFind;
+
+    @Autowired
+    public UserController(UserService userService, UserServiceFind userServiceFind) {
+        this.userService = userService;
+        this.userServiceFind = userServiceFind;
+    }
+
+    @GetMapping
+    public String getUserById (Principal principal, ModelMap modelMap){
+        User user = userServiceFind.findByUSerName(principal.getName());
+        System.out.println(user);
+        modelMap.addAttribute("user",user.getUsername()+" "+user
+                .getSurname()+" "+user.getSalary()+" "+user.getPassword());
+        return "user";
+    }
+}
